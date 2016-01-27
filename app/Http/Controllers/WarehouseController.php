dffd76d8406c0026c9fac1b7a725d6eec19a35a4
chelsymooy <chelsymooy1108@gmail.com>
2016-01-27 09:46:21 +0700

    repair warehouse returned data

--- app/Http/Controllers/WarehouseController.php
+++ app/Http/Controllers/WarehouseController.php
@@ -12,100 +12,132 @@ use Illuminate\Support\Facades\Input;
  */
 class WarehouseController extends Controller
 {
-    /**
-     * Display product stock's movement
-     *
-     * @param skip, take
-     * @return Response
-     */
-    public function card($id = null)
-    {
-        $result                     = \App\Models\TransactionDetail::varianid($id)->stockmovement(true);
+	/**
+	 * Display product stock's movement
+	 *
+	 * @param skip, take
+	 * @return Response
+	 */
+	public function card($id = null)
+	{
+		$varian                     = \App\Models\Varian::id($id);
 
-        $count                      = $result->count();
+		$detail                     = \App\Models\TransactionDetail::varianid($id)->stockmovement(true);
 
-        if(Input::has('skip'))
-        {
-            $skip                   = Input::get('skip');
-            $result                 = $result->skip($skip);
-        }
+		if(Input::has('search'))
+		{
+			$search                 = Input::get('search');
 
-        if(Input::has('take'))
-        {
-            $take                   = Input::get('take');
-            $result                 = $result->take($take);
-        }
+			foreach ($search as $key => $value) 
+			{
+				switch (strtolower($key)) 
+				{
+					case 'ondate':
+						$detail     = $detail->TransactionLogChangedAt($value);
+						$varian     = $varian->TransactionLogChangedAt($value);
+						break;
+					default:
+						# code...
+						break;
+				}
+			}
+		}
+   
+   		if(!$varian->first())
+   		{
+        	return new JSend('error', (array)Input::all(), 'ID Tidak valid.');
+   		}
 
-        $result                     = $result->with(['varian', 'varian.product'])->get()->toArray();
+		$varian                     = $varian->with(['product'])->first()->toArray();
 
-        return new JSend('success', (array)['count' => $count, 'data' => $result]);
-    }
+		$varian['details']          = $detail->get()->toArray();
 
-    /**
-     * Display products critical stock
-     *
-     * @param skip, take
-     * @return Response
-     */
-    public function critical()
-    {
-        $setting                    = \App\Models\Policy::ondate('now')->type('critical_stock')->first();
+		return new JSend('success', (array)$varian);
+	}
 
-        if(!$setting)
-        {
-            $critical               = 0;
-        }
-        else
-        {
-            $critical               = $setting['value'];
-        }
+	/**
+	 * Display products critical stock
+	 *
+	 * @param skip, take
+	 * @return Response
+	 */
+	public function critical()
+	{
+		$setting                    = \App\Models\Policy::ondate('now')->type('critical_stock')->first();
 
-        $result                     = \App\Models\Varian::critical($critical);
-        
-        $count                      = $result->count();
+		if(!$setting)
+		{
+			$critical               = 0;
+		}
+		else
+		{
+			$critical               = $setting['value'];
+		}
 
-        if(Input::has('skip'))
-        {
-            $skip                   = Input::get('skip');
-            $result                 = $result->skip($skip);
-        }
+		$result                     = \App\Models\Varian::critical($critical);
+		
+		$count                      = $result->count();
 
-        if(Input::has('take'))
-        {
-            $take                   = Input::get('take');
-            $result                 = $result->take($take);
-        }
+		if(Input::has('skip'))
+		{
+			$skip                   = Input::get('skip');
+			$result                 = $result->skip($skip);
+		}
 
-        $result                     = $result->with(['product'])->get()->toArray();
+		if(Input::has('take'))
+		{
+			$take                   = Input::get('take');
+			$result                 = $result->take($take);
+		}
 
-        return new JSend('success', (array)['count' => $count, 'data' => $result]);
-    }
+		$result                     = $result->with(['product'])->get()->toArray();
 
-    /**
-     * Display products critical stock
-     *
-     * @param skip, take
-     * @return Response
-     */
-    public function opname()
-    {
-        $result                     = \App\Models\Varian::with(['product']);
-        $count                      = $result->count();
-        
-        if(Input::has('skip'))
-        {
-            $skip                   = Input::get('skip');
-            $result                 = $result->skip($skip);
-        }
+		return new JSend('success', (array)['count' => $count, 'data' => $result]);
+	}
 
-        if(Input::has('take'))
-        {
-            $take                   = Input::get('take');
-            $result                 = $result->take($take);
-        }
+	/**
+	 * Display products critical stock
+	 *
+	 * @param skip, take
+	 * @return Response
+	 */
+	public function opname()
+	{
+		$result                     = \App\Models\Varian::with(['product']);
 
-        $result                     = $result->get()->toArray();
+		if(Input::has('search'))
+		{
+			$search                 = Input::get('search');
 
-        return new JSend('success', (array)['count' => $count, 'data' => $result]);
-    }
+			foreach ($search as $key => $value) 
+			{
+				switch (strtolower($key)) 
+				{
+					case 'ondate':
+						$result     = $result->TransactionLogChangedAt($value);
+						break;
+					default:
+						# code...
+						break;
+				}
+			}
+		}
+   
+		if(Input::has('skip'))
+		{
+			$skip                   = Input::get('skip');
+			$result                 = $result->skip($skip);
+		}
+
+		if(Input::has('take'))
+		{
+			$take                   = Input::get('take');
+			$result                 = $result->take($take);
+		}
+		$count                      = count($result->get(['id']));
+
+		$result                     = $result->get()->toArray();
+
+		return new JSend('success', (array)['count' => $count, 'data' => $result]);
+	}
 }
--- app/Models/Observers/TransactionLogObserver.php
+++ app/Models/Observers/TransactionLogObserver.php
@@ -10,218 +10,218 @@ use Carbon\Carbon;
  */
 class TransactionLogObserver 
 {
-    /** 
-     * observe transaction log event creating
-     * 1. modify changed at
-     * 2. act, accept or refuse
-     * 
-     * @param $model
-     * @return bool
-     */
-    public function creating($model)
-    {
-        $errors                         = new MessageBag();
+	/** 
+	 * observe transaction log event creating
+	 * 1. modify changed at
+	 * 2. act, accept or refuse
+	 * 
+	 * @param $model
+	 * @return bool
+	 */
+	public function creating($model)
+	{
+		$errors                         = new MessageBag();
 
-        //1. modify changed at
-        $model->changed_at              = Carbon::now()->format('Y-m-d H:i:s');
+		//1. modify changed at
+		$model->changed_at              = Carbon::now()->format('Y-m-d H:i:s');
 
-        return true;
-    }
-    /** 
-     * observe transaction log event saving
-     * 1. Check if transaction is sale
-     * 2. act, accept or refuse
-     * 
-     * @param $model
-     * @return bool
-     */
+		return true;
+	}
+	/** 
+	 * observe transaction log event saving
+	 * 1. Check if transaction is sale
+	 * 2. act, accept or refuse
+	 * 
+	 * @param $model
+	 * @return bool
+	 */
 	public function saving($model)
-    {
+	{
 		$errors 						= new MessageBag();
 
 		//A. Check if transaction is sale
-        if($model->sale()->count())
-        {
-            /** 
-            * Switch scheme
-            * 1. only allowed current status = abandoned if previous was cart
-            * 2. only allowed current status = cart if previous was non or cart
-            * 3. only allowed current status = wait if whole sale's items were sellable (available stock), and there were shipment address 
-            * 4. only allowed current status = paid/packed if bills = 0 
-            * 5. only allowed current status = shipping/delivery if receipt number not null 
-            * 6. only allowed current status = canceled if bills != 0 
-            */
-            switch($model->status)
-            {
-                case 'abandoned' :
-                    if($model->sale->status!='cart')
-                    {
-                        $errors->add('Log', 'Tidak dapat mengabaikan transaksi yang bukan di keranjang.');
-                    }
-                break;
-                case 'cart' :
-                    if($model->sale->status!='cart' && $model->sale->status!='na')
-                    {
-                        $errors->add('Log', 'Tidak dapat mengabaikan transaksi yang sudah checkout.');
-                    }
-                break;
-                case 'wait' :
-                    $details                = $model->sale->transactiondetails;
+		if($model->sale()->count())
+		{
+			/** 
+			* Switch scheme
+			* 1. only allowed current status = abandoned if previous was cart
+			* 2. only allowed current status = cart if previous was non or cart
+			* 3. only allowed current status = wait if whole sale's items were sellable (available stock), and there were shipment address 
+			* 4. only allowed current status = paid/packed if bills = 0 
+			* 5. only allowed current status = shipping/delivery if receipt number not null 
+			* 6. only allowed current status = canceled if bills != 0 
+			*/
+			switch($model->status)
+			{
+				case 'abandoned' :
+					if($model->sale->status!='cart')
+					{
+						$errors->add('Log', 'Tidak dapat mengabaikan transaksi yang bukan di keranjang.');
+					}
+				break;
+				case 'cart' :
+					if($model->sale->status!='cart' && $model->sale->status!='na')
+					{
+						$errors->add('Log', 'Tidak dapat mengabaikan transaksi yang sudah checkout.');
+					}
+				break;
+				case 'wait' :
+					$details                = $model->sale->transactiondetails;
 
-                    foreach ($details as $key => $value) 
-                    {
-                        if($value['varian']['current_stock'] < $value['quantity'])
-                        {
-                            $errors->add('Log', 'Stok '.$value['varian']['product']['name'].' ukuran '.$value['varian']['size']. ' tidak mencukupi');
-                        }
-                    }
-                    if(!$errors->count() && (!$model->sale->shipment()->count() || !$model->sale->shipment->address()->count()))
-                    {
-                        $errors->add('Log', 'Tidak dapat checkout tanpa alamat pengiriman.');
-                    }
-                break;
-                case 'paid' : case 'packed' :
-                    if(in_array($model->sale->status, ['cart']))
-                    {
-                        $errors->add('Log', 'Tidak dapat memvalidasi/packing transaksi yang bukan belum di checkout.');
-                    }
+					foreach ($details as $key => $value) 
+					{
+						if($value['varian']['current_stock'] < $value['quantity'])
+						{
+							$errors->add('Log', 'Stok '.$value['varian']['product']['name'].' ukuran '.$value['varian']['size']. ' tidak mencukupi');
+						}
+					}
+					if(!$errors->count() && (!$model->sale->shipment()->count() || !$model->sale->shipment->address()->count()))
+					{
+						$errors->add('Log', 'Tidak dapat checkout tanpa alamat pengiriman.');
+					}
+				break;
+				case 'paid' : case 'packed' :
+					if(in_array($model->sale->status, ['cart']))
+					{
+						$errors->add('Log', 'Tidak dapat memvalidasi/packing transaksi yang bukan belum di checkout.');
+					}
 
-                    if($model->sale->bills!=0)
-                    {
-                        $errors->add('Log', 'Pembayaran masih kurang sebesar '.$model->sale->bills.'.');
-                    }
-                break;
-                case 'shipping': case 'delivered' :
-                    if($model->sale->bills!=0)
-                    {
-                        $errors->add('Log', 'Pembayaran masih kurang sebesar '.$model->sale->bills.'.');
-                    }
+					if($model->sale->bills!=0)
+					{
+						$errors->add('Log', 'Pembayaran masih kurang sebesar '.$model->sale->bills.'.');
+					}
+				break;
+				case 'shipping': case 'delivered' :
+					if($model->sale->bills!=0)
+					{
+						$errors->add('Log', 'Pembayaran masih kurang sebesar '.$model->sale->bills.'.');
+					}
 
-                    if($model->type=='sell' && (!$model->sale->shipment()->count() || is_null($model->sale->shipment->receipt_number)))
-                    {
-                        $errors->add('Log', 'Tidak dapat checkout tanpa resi pengiriman.');
-                    }
-                break;
-                case 'canceled' :
-                    if($model->sale->status!='wait')
-                    {
-                        $errors->add('Log', 'Tidak dapat mengabaikan transaksi yang belum di checkout.');
-                    }
-                    elseif($model->sale->bills==0)
-                    {
-                        $errors->add('Log', 'Tidak dapat membatalkan transaksi yang sudah dibayar.');
-                    }
-                break;
-            }
-        }
-        
-        if($errors->count())
-        {
+					if($model->type=='sell' && (!$model->sale->shipment()->count() || is_null($model->sale->shipment->receipt_number)))
+					{
+						$errors->add('Log', 'Tidak dapat checkout tanpa resi pengiriman.');
+					}
+				break;
+				case 'canceled' :
+					if($model->sale->bills==0)
+					{
+						$errors->add('Log', 'Tidak dapat membatalkan transaksi yang sudah dibayar.');
+					}
+					elseif($model->sale->status!='wait')
+					{
+						$errors->add('Log', 'Tidak dapat mengabaikan transaksi yang belum di checkout.');
+					}
+				break;
+			}
+		}
+		
+		if($errors->count())
+		{
 			$model['errors'] 		= $errors;
 
-        	return false;
-        }
+			return false;
+		}
 
-        return true;
-    }
+		return true;
+	}
 
-    /** 
-     * observe transaction log event saved
-     * 1. Check if transaction is sale
-     * 2. act, accept or refuse
-     * 
-     * @param $model
-     * @return bool
-     */
-    public function saved($model)
-    {
-        $errors                                 = new MessageBag();
-        
-        //A. Check if transaction is sale
-        if($model->transaction()->count() && $model->transaction->type=='sell')
-        {
-           /**
-            * Switch scheme
-            * 1. current status = cart, save audit abandoned
-            * 2. current status = wait, credit point, if full-paid-points,  change status to paid
-            * 3. current status = paid, add quota and point for upline, then save audit payment
-            * 4. current status = shipping, save audit shipping
-            * 5. current status = delivered, save audit delivered
-            * 6. current status = canceled, revert point paid, save audit canceled
-            */
-            switch($model->status)
-            {
-                case 'cart' :
-                break;
-                case 'wait' :
-                    $result                     = $model->CreditPoint($model->sale);
+	/** 
+	 * observe transaction log event saved
+	 * 1. Check if transaction is sale
+	 * 2. act, accept or refuse
+	 * 
+	 * @param $model
+	 * @return bool
+	 */
+	public function saved($model)
+	{
+		$errors                                 = new MessageBag();
+		
+		//A. Check if transaction is sale
+		if($model->transaction()->count() && $model->transaction->type=='sell')
+		{
+		   /**
+			* Switch scheme
+			* 1. current status = cart, save audit abandoned
+			* 2. current status = wait, credit point, if full-paid-points,  change status to paid
+			* 3. current status = paid, add quota and point for upline, then save audit payment
+			* 4. current status = shipping, save audit shipping
+			* 5. current status = delivered, save audit delivered
+			* 6. current status = canceled, revert point paid, save audit canceled
+			*/
+			switch($model->status)
+			{
+				case 'cart' :
+				break;
+				case 'wait' :
+					$result                     = $model->CreditPoint($model->sale);
 
-                    if(!$result)
-                    {
-                        return false;
-                    }
+					if(!$result)
+					{
+						return false;
+					}
 
-                    if($model->sale->bills==0)
-                    {
-                        $result                 = $model->ChangeStatus($model->sale, 'paid');
-                    }
-                break;
-                case 'paid' :
-                    $result                     = $model->AddQuotaForUpline($model->sale);
+					if($model->sale->bills==0)
+					{
+						$result                 = $model->ChangeStatus($model->sale, 'paid');
+					}
+				break;
+				case 'paid' :
+					$result                     = $model->AddQuotaForUpline($model->sale);
 
-                    if(!$result)
-                    {
-                        return false;
-                    }
+					if(!$result)
+					{
+						return false;
+					}
 
-                    $result                 = $model->AddPointForUpline($model->sale);
-                break;
-                case 'shipping' :
-                break;
-                case 'delivered' :
-                break;
-                case 'canceled' :
-                    $result                     = $model->RevertPoint($model->sale);
-                break;
-            }
+					$result                 = $model->AddPointForUpline($model->sale);
+				break;
+				case 'shipping' :
+				break;
+				case 'delivered' :
+				break;
+				case 'canceled' :
+					$result                     = $model->RevertPoint($model->sale);
+				break;
+			}
 
-            if(isset($result) && !$result)
-            {
-                return false;
-            }
-        }
+			if(isset($result) && !$result)
+			{
+				return false;
+			}
+		}
 
-        if($errors->count())
-        {
-            $model['errors']        = $errors;
+		if($errors->count())
+		{
+			$model['errors']        = $errors;
 
-            return false;
-        }
+			return false;
+		}
 
-        return true;
-    }
-    
-    /** 
-     * observe transaction log event deleting
-     * 1. temporary refuse delete log
-     * 2. act, accept or refuse
-     * 
-     * @param $model
-     * @return bool
-     */
-    public function deleting($model)
-    {
+		return true;
+	}
+	
+	/** 
+	 * observe transaction log event deleting
+	 * 1. temporary refuse delete log
+	 * 2. act, accept or refuse
+	 * 
+	 * @param $model
+	 * @return bool
+	 */
+	public function deleting($model)
+	{
 		$errors 						= new MessageBag();
 
-        $errors->add('log', 'Tidak dapat menghapus log transaksi.');
+		$errors->add('log', 'Tidak dapat menghapus log transaksi.');
 
-        if($errors->count())
-        {
+		if($errors->count())
+		{
 			$model['errors'] 		= $errors;
 
-        	return false;
-        }
+			return false;
+		}
 
-        return true;
-    }
+		return true;
+	}
 }
