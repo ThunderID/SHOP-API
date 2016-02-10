2e72ee8ceb7fcd43f3d7b291759ea046aa173e6b
chelsymooy <chelsymooy1108@gmail.com>
2016-02-10 09:15:35 +0700

    Database migration preparation

--- database/migrations/2015_06_19_034756_create_stat_user_view_table.php
+++ database/migrations/2015_06_19_034756_create_stat_user_view_table.php
@@ -12,7 +12,7 @@ class CreateStatUserViewTable extends Migration
      */
     public function up()
     {
-        Schema::table('stat_user_views', function (Blueprint $table) {
+        Schema::create('stat_user_views', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('user_id')->unsigned()->index();
             $table->integer('statable_id')->unsigned()->index();
